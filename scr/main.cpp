8e2deedd57f2e26a4b874eefd4c86b8fc97a76f0
PythonGithCode <99050787+PythonGithCode@users.noreply.github.com>
2024-09-19 12:57:51 -0500

    Update main.cpp

--- scr/main.cpp
+++ scr/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
-using namespace std;
 
-int main() 
-{
-    cout << "Hello, World!";
-    return 0;
+#ifdef _WIN32
+#define EXPORT_SYMBOL __declspec(dllexport)
+#else
+#define EXPORT_SYMBOL
+#endif
+
+// A simple function to export
+extern "C" EXPORT_SYMBOL void hello() {
+    std::cout << "Hello from DLL!" << std::endl;
 }
+
