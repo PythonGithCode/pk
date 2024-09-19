e83cc940ca50d3ffa4fb4324d46eb450f2f60166
PythonGithCode <99050787+PythonGithCode@users.noreply.github.com>
2024-09-19 15:07:43 -0500

    Update mycode.cpp

--- scr/mycode.cpp
+++ scr/mycode.cpp
@@ -15,3 +15,12 @@ extern "C" EXPORT_SYMBOL void hello() {
 extern "C" __declspec(dllexport) void CALLBACK hellos(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow) {
     MessageBox(NULL, "Hello, World!", "My DLL", MB_OK);
 }
+
+extern "C" __declspec(dllexport) void CALLBACK hello2(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow) {
+    MessageBox(NULL, hwnd, "My DLL", MB_OK);
+}
+
+
+extern "C" __declspec(dllexport) void CALLBACK hello3(HWND hwnd, HINSTANCE hinst, LPSTR lpszCmdLine, int nCmdShow, String text) {
+    MessageBox(NULL, text, "My DLL", MB_OK);
+}
